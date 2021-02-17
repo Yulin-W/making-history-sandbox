6625124de00dd936d55c213d8ef1909794e4748c
Yulin-W <65204338+Yulin-W@users.noreply.github.com>
2021-02-18 08:43:56 +1100

    initialise cesium map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+// Import React
+import React from "react";
+
+// Import css
 import './App.css';
 
-function App() {
+// Import custom components
+import MapComponent from'./components/MapComponent.js';
+
+export default function App() {
   return (
     <div className="App">
-      Hello world
+      <MapComponent/>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/MapComponent.js
@@ -0,0 +1,11 @@
+// Import React
+import React from "react";
+
+// Import Resium
+import { Viewer } from "resium";
+
+export default function MapComponent() {
+    return (
+        <Viewer full/>
+    );
+}
