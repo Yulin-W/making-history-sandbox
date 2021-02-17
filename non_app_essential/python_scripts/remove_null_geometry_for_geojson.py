58fbf715c279f6b6e0ecf83023389b32a43c072c
Yulin-W <65204338+Yulin-W@users.noreply.github.com>
2021-02-18 10:51:32 +1100

    Fixed geojson load range error by removing polygon for antarctica
    I assume it was due to antarctica being so large that it had too many coodinates for its multipolygon

---
+++ non_app_essential/python_scripts/remove_null_geometry_for_geojson.py
@@ -0,0 +1,15 @@
+import json
+
+f = open(r'C:\Users\wujun\OneDrive\Documents\GitHub\making-history-sandbox\src\assets\basemap\mapAdmin.json',"r", encoding="utf8") # Fill in your input file here
+  
+data = json.load(f) 
+
+print(data["type"])
+
+
+# Remove feature if its geometry is null in json
+data["features"] = [feature for feature in data["features"] if feature["geometry"]];
+
+fs = open('mapAdminBetter.json',"w", encoding="utf8") # Fill in your output file here
+
+json.dump(data, fs, ensure_ascii=False)
--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -2,10 +2,17 @@
 import React from "react";
 
 // Import Resium
-import { Viewer } from "resium";
+import { Viewer, GeoJsonDataSource } from "resium";
+
+// Import default basemap geojson
+import mapAdmin from "../assets/basemap/mapAdmin.json";
+console.log(mapAdmin.features.length);
 
 export default function MapComponent() {
     return (
-        <Viewer full/>
+        <Viewer full>
+            {/*full ensures map takes full screen*/}
+            <GeoJsonDataSource data={mapAdmin}/>
+        </Viewer>
     );
 }
