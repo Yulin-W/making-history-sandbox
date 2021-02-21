8e60d2383f7de4b331a4a5e1fa5f84bd6fcd28f0
Yulin-W <65204338+Yulin-W@users.noreply.github.com>
2021-02-21 21:31:51 +1100

    completed saveScenario script

---
+++ src/scripts/saveScenario.js
@@ -0,0 +1,7 @@
+import { saveAs } from 'file-saver';
+
+// Expects an generic javascript object, one that is compatible with JSON.stringify and reading
+export default function saveScenario(obj) {
+    const blob = new Blob([JSON.stringify(obj)], {type: "application/json;charset=utf-8"});
+    saveAs(blob, "scenarioSave.json");
+};
