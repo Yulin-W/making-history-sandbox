fdfbbd8892753340e2e68946b74d22571eb17137
Yulin-W <65204338+Yulin-W@users.noreply.github.com>
2021-02-22 16:01:03 +1100

    Update LassoComponent.js
    add more validification of selection of regions

--- src/components/LassoComponent.js
+++ src/components/LassoComponent.js
@@ -7,18 +7,21 @@ export default function LassoComponent(props) {
     // Event handler for when lasso selection is complete
     const eventHandler = event => {
         let indices = [];
-        event.layers.forEach(layer => {
-            if (layer) { // Series of if statements to only record in indices the valid selected regions TODO: this seems a bit dodgy
-                if (layer.feature) {
-                    if (layer.feature.properties) {
-                        if (layer.feature.properties.regionID) {
-                            console.log(layer);
-                            indices.push(layer.feature.properties.regionID);
+        if (event) { // Series of if statements to only record in indices the valid selected regions TODO: this seems a bit dodgy, problem is this is like one of those silently neglect mistakes types of code
+            if (event.layers) {
+                event.layers.forEach(layer => {
+                    if (layer) {
+                        if (layer.feature) {
+                            if (layer.feature.properties) {
+                                if (layer.feature.properties.regionID) {
+                                    indices.push(layer.feature.properties.regionID);
+                                }
+                            }
                         }
                     }
-                }
+                });       
             }
-        });
+        }
         props.updateLassoSelecting(false, () => {props.assignRegions(indices);})
     };
 
@@ -32,8 +35,8 @@ export default function LassoComponent(props) {
         // Execute region coloring upon lasso selection completion
         map.addEventListener('lasso.finished', eventHandler);
 
-        return (() => {
-            lasso.disable();
+        return (() => { // What to do on unmounting the LassoComponent
+            lasso.disable(); // FIXME: bug: disabling via this salong, i.e. in the case of disabling via using the cancel button, appears to lead to the drawing of selection persisting, though that selection has no coloring effect
             map.removeEventListener('lasso.finished', eventHandler); // Remove event handler to avoid creating multiple handlers
         });
     }, [map]);
