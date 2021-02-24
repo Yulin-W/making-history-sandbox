b7369890d6dcaee8576f8c89fb0572818e2743e1
Yulin-W <65204338+Yulin-W@users.noreply.github.com>
2021-02-24 20:11:13 +1100

    add tonerbackground as a background map

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -9,6 +9,9 @@ import "leaflet/dist/leaflet.css";
 // Import relevant custom components for plugins
 import LassoComponent from "./LassoComponent.js";
 
+// Import mapProviders settings
+import mapProviders from '../settings/mapProviders.js';
+
 const useStyles = theme => ({
     mapContainer: {
         position: "absolute",
@@ -20,35 +23,6 @@ const useStyles = theme => ({
         filter: "brightness(1) contrast(100%)",
     },
 });
-
-// Add leaflet providers here
-const mapProviders = [
-    {
-        name: "Esri.WorldImagery",
-        attr: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
-        src: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
-    },
-    {
-        name: "OpenTopoMap",
-        attr: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
-        src: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
-    },
-    {
-        name: "OpenStreetMap.Mapnik",
-        attr: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        src: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    },
-    {
-        name: "Stamen.TerrainBackground",
-        attr: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        src: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.png'
-    },
-    {
-        name: "Stamen.Watercolor",
-        attr: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        src: 'https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png'
-    },
-]
 class MapComponent extends React.PureComponent {
     constructor(props) {
         super(props);
---
+++ src/settings/mapProviders.js
@@ -0,0 +1,35 @@
+// Add leaflet providers here
+const mapProviders = [
+    {
+        name: "Esri.WorldImagery",
+        attr: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
+        src: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
+    },
+    {
+        name: "OpenTopoMap",
+        attr: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+        src: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png'
+    },
+    {
+        name: "OpenStreetMap.Mapnik",
+        attr: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        src: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+    },
+    {
+        name: "Stamen.TerrainBackground",
+        attr: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        src: 'https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.png'
+    },
+    {
+        name: "Stamen.Watercolor",
+        attr: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        src: 'https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.png'
+    },
+    {
+        name: "Stamen.TonerBackground",
+        attr: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        src: 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-background/{z}/{x}/{y}{r}.png'
+    },
+]
+
+export default mapProviders;
