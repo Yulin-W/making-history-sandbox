512935583d1b146bb9479fc9d65b8fe46e051bc8
Yulin-W <65204338+Yulin-W@users.noreply.github.com>
2021-03-07 11:14:00 +1100

    hiding and showing panels complete

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,6 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import loadScenario from '../scripts/loadScenario.js';
 import Dropzone from 'react-dropzone';
+import Slide from '@material-ui/core/Slide';
+
+// Import retract button custom component
+import RetractButton from './RetractButton.js';
 
 const useStyles = makeStyles((theme) => ({
     menuContainer: {
@@ -31,27 +35,31 @@ const useStyles = makeStyles((theme) => ({
 
 function MenuComponent(props) {
     const classes = useStyles();
+    const [display, setDisplay] = React.useState(true);
     return (
-        <div className={classes.menuContainer} id="menu">
-            <Button size="small" className={classes.menuButton} onClick={() => window.location.reload()}>New</Button>
-            <Button size="small" className={classes.menuButton} onClick={props.save}>Save</Button>
-            <Dropzone
-                onDrop={acceptedFiles => {
-                    loadScenario(acceptedFiles[0], props.loadSave);
-                }}
-            >
-                {/*acceptedFiles[0] as we only care about a single file TODO: make some restriction to filetype (.json) to upload and number of files to upload (1 only)*/}
-                {/*TODO: Here we used a drop zone, this might be an overkill to be honest as I only need a load button, so if performance drops too much or too much bandwidth taken, try to simplify this*/}
-                {({ getRootProps, getInputProps }) => (
-                    <div {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        <Button size="small" className={classes.menuButton}>Load</Button>
-                        {/*This is a dummy button for visuals only, the uploading functionality is contained in the Dropzone and the input element*/}
-                    </div>
-                )}
+        <Slide direction="down" in={display} unmountOnExit={false} mountOnEnter={false}>
+            <div className={classes.menuContainer} id="menu">
+                <Button size="small" className={classes.menuButton} onClick={() => window.location.reload()}>New</Button>
+                <Button size="small" className={classes.menuButton} onClick={props.save}>Save</Button>
+                <Dropzone
+                    onDrop={acceptedFiles => {
+                        loadScenario(acceptedFiles[0], props.loadSave);
+                    }}
+                >
+                    {/*acceptedFiles[0] as we only care about a single file TODO: make some restriction to filetype (.json) to upload and number of files to upload (1 only)*/}
+                    {/*TODO: Here we used a drop zone, this might be an overkill to be honest as I only need a load button, so if performance drops too much or too much bandwidth taken, try to simplify this*/}
+                    {({ getRootProps, getInputProps }) => (
+                        <div {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            <Button size="small" className={classes.menuButton}>Load</Button>
+                            {/*This is a dummy button for visuals only, the uploading functionality is contained in the Dropzone and the input element*/}
+                        </div>
+                    )}
                 </Dropzone>
-            <Button size="small" className={classes.menuButton} onClick={props.openHelp}>Help</Button>
-        </div>
+                <Button size="small" className={classes.menuButton} onClick={props.openHelp}>Help</Button>
+                <RetractButton direction="up" top={31} left={"50%"} checked={display} onClick={() => setDisplay(!display)} />
+            </div>
+        </Slide>
     );
 }
 
---
+++ src/components/RetractButton.js
@@ -0,0 +1,49 @@
+// Import React
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import UpIcon from '@material-ui/icons/KeyboardArrowUp';
+import DownIcon from '@material-ui/icons/KeyboardArrowDown';
+
+const transformDict = {
+    "up": "rotate(0deg)",
+    "down": "rotate(180deg)",
+    "right": "rotate(90deg)",
+    "left": "rotate(270deg)",
+}
+
+const useStyles = makeStyles((theme) => ({
+    retractButton: {
+        position: "absolute",
+        width: 100,
+        height: 10,
+        backgroundColor: theme.palette.background.light,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        textAlign: "center",
+        marginLeft: -50,
+        marginTop: -5,
+        visibility: "visible", // ensures even with say transformations where visibility is hidden after transforming, that the button is still visible
+    }
+}));
+
+function RetractButton(props) {
+    const classes = useStyles();
+    const transform = transformDict[props.direction];
+    const Icon = props.checked ? UpIcon : DownIcon;
+    return (
+        <div
+            className={classes.retractButton}
+            onClick={props.onClick}
+            style={{
+                transform: "scale(0.6)" + transform,
+                top: props.top,
+                left: props.left,
+            }}
+        >
+            <Icon size="small"/>
+        </div>
+    );
+}
+
+export default RetractButton;
