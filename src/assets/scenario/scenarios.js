4c61f6d088d403c5c37502ab419b5c67ec506e9c
Yulin-W <65204338+Yulin-W@users.noreply.github.com>
2021-10-11 15:40:28 +1100

    Add 117 scenario

--- src/assets/scenario/scenarios.js
+++ src/assets/scenario/scenarios.js
@@ -1,5 +1,6 @@
 import scen2800BC from "./2800BC-Epic-of-Gilgamesh.json";
 import scen356BC from "./356BC-Birth-of-Alexander.json";
+import scen117 from "./117-Pax-Romana.json";
 import scen867 from "./867-Macedonian-Dynasty.json";
 import scen1206 from "./1206-Rise-of-Mongolia.json";
 import scen1444 from "./1444-Battle-of-Varna.json";
@@ -9,6 +10,7 @@ import scen1936 from "./1936-Coming-of-the-Storm.json"
 const scenarios = {
   "2800BC Epic of Gilgamesh": scen2800BC,
   "356BC Birth of Alexander": scen356BC,
+  "117-Pax-Romana": scen117,
   "867 Macedonian Dynasty": scen867,
   "1206 Rise of Mongolia": scen1206,
   "1444 Battle of Varna": scen1444,
