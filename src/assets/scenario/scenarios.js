30c46ec65cfbf001721dd5cf9db6da0c22ef8d3a
Yulin-W <65204338+Yulin-W@users.noreply.github.com>
2021-10-11 15:01:59 +1100

    Add 2800BC scenario

--- src/assets/scenario/scenarios.js
+++ src/assets/scenario/scenarios.js
@@ -1,3 +1,4 @@
+import scen2800BC from "./2800BC-Epic-of-Gilgamesh.json";
 import scen356BC from "./356BC-Birth-of-Alexander.json";
 import scen867 from "./867-Macedonian-Dynasty.json";
 import scen1206 from "./1206-Rise-of-Mongolia.json";
@@ -6,6 +7,7 @@ import scen1936 from "./1936-Coming-of-the-Storm.json"
 
 // Import json scenarios, keys should be names that will be displayed for the scenario
 const scenarios = {
+  "2800BC Epic of Gilgamesh": scen2800BC,
   "356BC Birth of Alexander": scen356BC,
   "867 Macedonian Dynasty": scen867,
   "1206 Rise of Mongolia": scen1206,
